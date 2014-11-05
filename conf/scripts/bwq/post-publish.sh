bff2e3f8ec01e8d4d7ddc757914107246aedab92
der <dave@epimorphics.com>
2014-11-05 17:30:08 +0000

    partial development of post-publish scripts

--- conf/scripts/bwq/post-publish.sh
+++ conf/scripts/bwq/post-publish.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 # Post-publication actions for the BWQ service
-echo "Post-publication script called on $1"
+# Arguments:
+#     serversDir
+#     server
+
+set -o errexit
+[[ $# = 2 ]] || { echo "Internal error calling $0.sh" 1>&2 ; exit 1 ; }
+. ./config.sh
+
+readonly serversDir="/var/opt/dms/$1"
+readonly server="$2"
+
+echo "Post-publication script called on $server"
+
+readonly IP=$( jq -r .address "$serversDir/servers/$server/config.json" )
+
+rsync -a --delete -e "ssh $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem" "$serversDir/../../Web" ubuntu@$IP:/var/www/environment/html
