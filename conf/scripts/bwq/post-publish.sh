390fe700593fe3feb95633d0356f11f151d1c8b5
der <dave@epimorphics.com>
2014-11-06 22:48:34 +0000

    fixes to post publish

--- conf/scripts/bwq/post-publish.sh
+++ conf/scripts/bwq/post-publish.sh
@@ -10,11 +10,17 @@ set -o errexit
 
 readonly serversDir="/var/opt/dms/$1"
 
-echo "Synchronized web content, including source/dump files"
+echo "Synchronizing web content, including source/dump files"
 cd $serversDir/../..
 for server in tiers/presServers/servers/* 
 do
+    FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem"
     echo "Sync to $server"
     IP=$( jq -r .address "$server/config.json" )
-    rsync -a --delete -e "ssh $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem" Web ubuntu@$IP:/var/www/environment/html
+    rsync -a --delete -e "ssh $FLAGS" Web ubuntu@$IP:/var/www/environment/html
+
+    echo "Clear caches"
+    ssh -t -t $FLAGS -l ubuntu $IP sudo /usr/local/bin/ps_cache_clean 
+    # ssh -t -t $FLAGS -l ubuntu $IP sudo /usr/sbin/htcacheclean -l1b -t -p/var/cache/apache2/mod_cache_disk
 done
+
