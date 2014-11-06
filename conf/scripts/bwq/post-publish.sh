1219b6307017af001c55f59c1b0e487ac082e8a1
der <dave@epimorphics.com>
2014-11-06 17:54:57 +0000

    Modified to new post-processing call format

--- conf/scripts/bwq/post-publish.sh
+++ conf/scripts/bwq/post-publish.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # Post-publication actions for the BWQ service
 # Arguments:
-#     serversDir
-#     server
+#     serversDir (for data servers)
+
 
 set -o errexit
-[[ $# = 2 ]] || { echo "Internal error calling $0.sh" 1>&2 ; exit 1 ; }
+[[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
 . ./config.sh
 
 readonly serversDir="/var/opt/dms/$1"
-readonly server="$2"
 
-echo "Post-publication script called on $server"
-
-readonly IP=$( jq -r .address "$serversDir/servers/$server/config.json" )
-
-rsync -a --delete -e "ssh $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem" "$serversDir/../../Web" ubuntu@$IP:/var/www/environment/html
+echo "Synchronized web content, including source/dump files"
+cd $serversDir/../..
+for server in tiers/presServers/servers/* 
+do
+    echo "Sync to $server"
+    IP=$( jq -r .address "$server/config.json" )
+    rsync -a --delete -e "ssh $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem" Web ubuntu@$IP:/var/www/environment/html
+done
