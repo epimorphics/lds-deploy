b2cbf3f2dcb6e8f75ff4e09b3e18ad5e2069f7aa
der <dave@epimorphics.com>
2014-11-08 15:02:48 +0000

    Fixing Web sync error

--- conf/scripts/bwq/post-publish.sh
+++ conf/scripts/bwq/post-publish.sh
@@ -11,15 +11,15 @@ set -o errexit
 readonly serversDir="/var/opt/dms/$1"
 
 echo "Synchronizing web content, including source/dump files"
-cd $serversDir/../..
-for server in tiers/presServers/servers/* 
+cd $serversDir/../../Web
+for server in ../tiers/presServers/servers/* 
 do
     if grep -qv Terminated $server/status 
     then
         FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem"
         echo "Sync to $server"
         IP=$( jq -r .address "$server/config.json" )
-        rsync -a --delete -e "ssh $FLAGS" Web ubuntu@$IP:/var/www/environment/html
+        rsync -a --delete -e "ssh $FLAGS" * ubuntu@$IP:/var/www/environment/html
 
         echo "Clear caches"
         ssh -t -t $FLAGS -l ubuntu $IP sudo /usr/local/bin/ps_cache_clean 
