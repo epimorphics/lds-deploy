e071435f9a31fee4909c8ee399d1dd7510bf0163
der <dave@epimorphics.com>
2014-11-13 12:04:59 +0000

    changing DB upload directory, to help with disk management

--- conf/scripts/dbutil/load-db.sh
+++ conf/scripts/dbutil/load-db.sh
@@ -10,13 +10,14 @@ set -o errexit
 readonly serverDir=$1
 readonly imageFile=$2
 readonly fileName=$(basename $imageFile)
+readonly remoteDir=/home/ubuntu/deploy/dbload
 
 IP=$(jq -r ".Instances[0].PublicDnsName" < $serverDir/aws-instance.json)
 
 echo "Uploading $fileName to server $IP"
-scp -Cq $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem $imageFile ubuntu@${IP}:/tmp
+scp -Cq $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem $imageFile ubuntu@${IP}:$remoteDir
 
-ssh $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP "sudo bash /usr/local/bin/dbinstall /tmp/$fileName"
+ssh $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP "sudo bash /usr/local/bin/dbinstall $remoteDir/$fileName"
 
 # Delay a little to allow remote server to restart
 sleep 8s
