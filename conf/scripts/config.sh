042f373094826a814cee0f47782b822d474704a3
der <dave@epimorphics.com>
2015-03-04 17:24:56 +0000

    Updating ubuntu amis

--- conf/scripts/config.sh
+++ conf/scripts/config.sh
@@ -2,9 +2,9 @@
 
 export AWS_DEFAULT_REGION=eu-west-1
 
-readonly AMI_UBUNTU_EBS=ami-096cbd7e        # ubuntu 14.04, 64bit, ebs root
+readonly AMI_UBUNTU_EBS=ami-cda130ba        # ubuntu 14.04, 64bit, ebs root
 readonly AMI_UBUNTU_INSTANCE=ami-3b69b84c   # ubuntu 14.04, 64bit, instance root
-readonly AMI_UBUNTU_HVM=ami-fd6cbd8a        # ubuntu 14.04, 64bit, HVM (for use with t2)
+readonly AMI_UBUNTU_HVM=ami-c5bf2eb2        # ubuntu 14.04, 64bit, HVM (for use with t2)
 readonly SG_BACK_END=sg-761ac813            # security group for lds-back-end
 readonly SG_FRONT_END=sg-b815c7dd           # security group for lds-front-end
 readonly VPC_PUBLIC_B=subnet-5b73601d       # public subnet for LDS VPC
