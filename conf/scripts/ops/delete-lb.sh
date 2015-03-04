6dd848a91b46143b3ad94eac076226c4ab2ed4fd
der <dave@epimorphics.com>
2015-03-04 14:51:55 +0000

    Add recording of LB status to the S3 state

--- conf/scripts/ops/delete-lb.sh
+++ conf/scripts/ops/delete-lb.sh
@@ -15,6 +15,14 @@ if [[ -f $tierDir/lb-name ]]; then
     echo "Deleting load balancer $NAME"
     aws elb delete-load-balancer --load-balancer-name "$NAME"
     rm $tierDir/aws-lb.json $tierDir/lb-name
+
+    # Record status in S3 status already
+    if [[ $tierDir =~ /var/opt/dms/(.*) ]]; then
+        s3key="$S3_STATE/${BASH_REMATCH[1]}"
+        aws s3 rm "$tierDir/lb-name"
+        aws s3 rm "$tierDir/aws-lb.json"
+    fi
+
 else
     echo "No load balancer found" 1>&2
 fi
