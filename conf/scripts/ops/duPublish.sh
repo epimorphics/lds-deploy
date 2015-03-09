956c30542210b1288d68da8c38c302e03044c208
der <dave@epimorphics.com>
2015-03-09 22:52:37 +0000

    Experimenting with S3-based publishing

---
+++ conf/scripts/ops/duPublish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# TODO code tpu kick server to pull external state from S3
---
+++ conf/scripts/ops/duPublishState.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# TODO code to convert tier level publish to an S3 upload
+# S3 area passed in as an ENV param
