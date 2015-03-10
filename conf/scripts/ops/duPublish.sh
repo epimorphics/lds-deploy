98bbbebdc4ab3fd018ac093e6a27be9e0d91b151
der <dave@epimorphics.com>
2015-03-10 15:36:32 +0000

    config and dummy scripts for test and dev of publishing via S3 state

--- conf/scripts/ops/duPublish.sh
+++ conf/scripts/ops/duPublish.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # TODO code tpu kick server to pull external state from S3
+echo "Publish called on $*"
--- conf/scripts/ops/duPublishState.sh
+++ conf/scripts/ops/duPublishState.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # TODO code to convert tier level publish to an S3 upload
 # S3 area passed in as an ENV param
+echo "Publish state called on $*"
+echo "State folder is $STATE_FOLDER"
