3e15aeb2fd841e777f019a3d116e6aa80e97b22f
der <dave@epimorphics.com>
2015-03-13 21:43:58 +0000

    Adding support for publishing via S3 and configuring test service to use this

--- conf/scripts/ops/duPublish.sh
+++ conf/scripts/ops/duPublish.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# TODO code tpu kick server to pull external state from S3
+# Call data server to pull updated state from S3
+
+set -o errexit
 echo "Publish called on $*"
+
+[[ $# = 2 ]] || { echo "Usage: duPublish serverDir" 1>&2 ; exit 1 ; }
+serverDir="/var/opt/dms/$1/servers/$2"
+
+. ./config.sh
+
+IP=$( jq -r .address "$serverDir/config.json" )
+
+echo "Calling dms-update on $serverDir"
+ssh -t -t $FLAGS -l ubuntu $IP /bin/bash /usr/local/bin/dms-update --perform
