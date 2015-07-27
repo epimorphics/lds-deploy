bfaaae9d3fcb2d81b1700331340223bdf1ee01ad
der <dave@epimorphics.com>
2015-07-27 21:23:01 +0100

    Add tier reset to prototype epr monitor script

---
+++ conf/scripts/dbutil/resetTierData.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Reset the data for all servers in a data tier
+# For use from within DMS (must be run as tomcat user)
+# args: tierDir or pass in from environment
+
+set -o errexit
+
+tierDir
+if [[ $# = 1 ]]; 
+then
+    tierDir = "$1"
+fi
+[[ -n $tierDir ]] || { echo "Internal error calling resetTierData.sh, no tier given" 1>&2 ; exit 1 ; }
+
+. /opt/dms/conf/scripts/config.sh
+FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem"
+
+for server in $tierDir/servers/*
+do
+    if grep -qv Terminated $server/status 
+        then
+        IP=$( jq -r .address "$server/config.json" )
+        echo "Calling db_reset on $server"
+        ssh -t -t $FLAGS -l ubuntu $IP /bin/bash /usr/local/bin/db_reset
+    fi
+done
