1b355079388efca4af96120d36e88efcc7eb0d32
der <dave@epimorphics.com>
2015-08-06 15:51:36 +0100

    Fix tests, use arithmetic instead of lexical comparisons

--- conf/scripts/dbutil/resetTierData.sh
+++ conf/scripts/dbutil/resetTierData.sh
@@ -28,6 +28,7 @@ do
             service="${BASH_REMATCH[1]}"
             testRunner="/opt/dms/conf/tests/$service/runtests.sh"
             if [[ -x "$testRunner" ]]; then
+                echo "Runing integration test on $server"
                 $testRunner $server || { echo "Tests failed, aborting with $server out of LB" 1>&2 ; exit 1 ; }
             fi
         fi
