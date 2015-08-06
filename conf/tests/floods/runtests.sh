e480f423be09b83425c018694d64cf3973265b0b
der <dave@epimorphics.com>
2015-08-06 14:41:25 +0100

    Adding test case for floods server

---
+++ conf/tests/floods/runtests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Integration tests for a floods server
+# Assumes run by tomcat7 user
+# Argument:address of the server under test
+
+set -o errexit
+set -o pipefail
+
+[[ $# = 1 ]] || { echo "Internal error calling runtests, expected server address" 1>&2 ; exit 1 ; }
+
+readonly SERVER="$1"
+readonly IP=$( jq -r .address "$SERVER/config.json" )
+
+probe() {
+    curl -s "http://$IP/flood-monitoring/$1"
+}
+
+report_error() {
+    echo "Test failed: $1"
+    exit 1
+}
+
+[[ $( probe "data/readings.csv?latest" | wc -l ) > 1000 ]] || report_error "Not enough readings"
+[[ $( probe "id/floods" ) != "null" ]]                     || report_error "No items in flood warnings"
+[[ $( probe "id/floodAreas?_limit=10" | wc -l ) > 9 ]]     || report_error "Not enough flood areas"
