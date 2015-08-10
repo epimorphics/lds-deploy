c5c20303e342923280b7b1648df690c2aac635eb
der <dave@epimorphics.com>
2015-08-10 15:35:38 +0100

    Adding server tests and upgrade actions

---
+++ conf/scripts/ops/upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Full update - assumes update testing on some dev/test machine 
+
+set -o errexit
+
+[[ $# = 1 ]] || { echo "Internal error calling $0, expected server address" 1>&2 ; exit 1 ; }
+
+readonly SERVER="$1"
+readonly IP=$( jq -r .address "$SERVER/config.json" )
+
+. /opt/dms/conf/scripts/config.sh
+
+ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP apt-get -yq update
+ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP apt-get -yq upgrade
+ssh -t -t $SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem -l ubuntu $IP apt-get -yq dist-upgrade
