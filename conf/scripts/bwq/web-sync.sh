8b11441fa6d3da37a292e8e68de78b6ca1da5652
der <dave@epimorphics.com>
2015-08-11 09:27:29 +0100

    Fix server activate web sync

--- conf/scripts/bwq/web-sync.sh
+++ conf/scripts/bwq/web-sync.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Web sync operation
+# Web sync operation for whole tier
 # Arguments:
-#     serversDir (for presentation servers)
+#     tier (for presentation servers)
 
 
 set -o errexit
 [[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
 . ./config.sh
 
-readonly serversDir="$1"
+readonly tierDir="$1"
 
 echo "Synchronizing web content, including source/dump files"
-cd $serversDir/../../Web
-for server in $serversDir/servers/* 
+cd $tierDir/../../Web
+for server in $tierDir/servers/* 
 do
     if grep -qv Terminated $server/status 
     then
