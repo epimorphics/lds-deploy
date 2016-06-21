78da3ffa89b017fd44cb000e39f5a4b6186bed40
der <dave@epimorphics.com>
2016-06-21 16:12:04 +0100

    Add initial CDE service

---
+++ conf/scripts/cde/web-sync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Web sync operation for whole tier
+# Arguments:
+#     tier (for presentation servers)
+
+
+set -o errexit
+[[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
+. ./config.sh
+
+readonly tierDir="$1"
+
+echo "Synchronizing web content, including source/dump files"
+cd $tierDir/../../Web
+for server in $tierDir/servers/* 
+do
+    if grep -qv Terminated $server/status 
+    then
+        FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem"
+        echo "Sync to $server"
+        IP=$( jq -r .address "$server/config.json" )
+        rsync -a --delete -e "ssh $FLAGS" * ubuntu@$IP:/var/www/cde/html
+    fi
+done
