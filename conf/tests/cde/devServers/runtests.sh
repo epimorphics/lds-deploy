563523012030a19451fa6d4f56e654f0910b791b
der <dave@epimorphics.com>
2016-08-14 19:22:35 +0100

    Run cucumber tests headless

--- conf/tests/cde/devServers/runtests.sh
+++ conf/tests/cde/devServers/runtests.sh
@@ -7,4 +7,4 @@ readonly IP=$( jq -r .address "$SERVER/config.json" )
 
 export TEST_HOST=$IP
 cd /opt/dms/conf/tests/cde/ea_cde_pub/test/cucumber
-/usr/local/bin/bundle exec /usr/local/bin/cucumber
+xvfb-run -a /usr/local/bin/bundle exec /usr/local/bin/cucumber
