c3d82f0755f8f6c984f7fdc19638cf38e58579ed
der <dave@epimorphics.com>
2017-02-02 09:17:51 +0000

    Support for monitoring apache stuck processe

---
+++ conf/scripts/ops/serverMonitor.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Check apache status for stuck processes, force apache restart if two processes stalled
+#  serverMonitor.sh tierDir
+
+set -o errexit
+
+[[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
+readonly tierDir="$1"
+readonly SERVERS_BASE=$tierDir/servers
+
+cd /opt/dms/conf/scripts
+. ./config.sh
+. ./lib.sh
+. ./automation_lib.sh
+
+readonly TOPIC_ARN=arn:aws:sns:eu-west-1:853478862498:Nagios-alerts
+
+checkServer() {
+    [[ $# = 1 ]] || { echo "Internal error calling checkServer" 1>&2 ; exit 1 ; }
+    local server=$1
+    IP=$( jq -r .address "$server/config.json" )
+    ssh $SSH_FLAGS -i /var/opt/dms/.ssh/lds-user.pem -l ubuntu $IP "curl -s http://localhost/server-status | tr '\n' ' ' | grep -o '<pre>.*</pre>' | grep -o W | wc -l"
+}
+
+apacheRestart() {
+    [[ $# = 1 ]] || { echo "Internal error calling apacheRestart" 1>&2 ; exit 1 ; }
+    local server=$1
+    IP=$( jq -r .address "$server/config.json" )
+    ssh -t -t $FLAGS -l ubuntu $IP sudo service apache2 restart
+}
+
+for server in $SERVERS_BASE/*
+do
+    if grep -q Running $server/status 
+    then
+        count=$( checkServer $server )
+        echo "$date Apache write count = $count for $server"
+        if (( $count > 40 )); then
+            echo "$date restarting apache on $server"
+            suspendServer $server
+            apacheRestart $server
+            unsuspendServer $server
+            aws sns publish --topic-arn $TOPIC_ARN --subject "Floods server warning" --message "Stuck thread count of $count, restarting apache on $server"
+        fi
+    fi
+done
