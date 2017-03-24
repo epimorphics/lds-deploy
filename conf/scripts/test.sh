94c92a073add1d346499b084a88977a7df931cc7
der <dave.reynolds@epimorphics.com>
2017-03-24 08:50:08 +0000

    Testing WIMS api access

--- conf/scripts/test.sh
+++ conf/scripts/test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-WaitFor() {
-    local loop=1
-    while [[ $loop -le 100 ]]
-    do
-        echo "Hello $loop"
-        echo "Hello $loop" >> /tmp/messages
-        sleep 1
-        loop=$(( $loop + 1 ))
-    done
-}
+echo "Tests script called with: $@"
 
-WaitFor
-echo "Exiting"
+echo "Environment is:"
+env
